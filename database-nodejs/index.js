6600ab0a47b091f1b57509adc71b0f90e18a693f
Ashu-172 <mishraashutosh801@gmail.com>
2020-10-02 09:21:07 +0530

    Formatting

--- database-nodejs/index.js
+++ database-nodejs/index.js
@@ -1,49 +1,51 @@
-const mysql = require("mysql2/promise")
+const mysql = require("mysql2/promise");
 
 connect();
 
-async function connect(){
-    try{
-        const conn = await mysql.createConnection({
-            "host" : "192.168.43.69",
-            "port" : 3306,
-            "user" : "ashutosh",
-            "password" : "Ashutosh@1",
-            "database" : "testdb"
-        });
+async function connect() {
+  try {
+    const conn = await mysql.createConnection({
+      host: "192.168.43.69",
+      port: 3306,
+      user: "ashutosh",
+      password: "Ashutosh@1",
+      database: "testdb",
+    });
 
-        await conn.beginTransaction();
+    await conn.beginTransaction();
 
-        const resultDelete = await conn.query(
-            "delete from employees where name = ?",["Ashwini Mishra"]
-        )
-        console.log(resultDelete[0].affectedRows);
+    const resultDelete = await conn.query(
+      "delete from employees where name = ?",
+      ["Ashwini Mishra"]
+    );
+    console.log(resultDelete[0].affectedRows);
 
-        const resultInsert = await conn.query(
-            "insert into employees (id,name,ssn) values ( ?, ?, ?)",
-            [6,"Ashwini","116"]
-        );
-        console.log(resultInsert[0].affectedRows);
+    const resultInsert = await conn.query(
+      "insert into employees (id,name,ssn) values ( ?, ?, ?)",
+      [6, "Ashwini", "116"]
+    );
+    console.log(resultInsert[0].affectedRows);
 
-        const result = await conn.query("select * from employees");
-        console.table(result[0]);
+    const result = await conn.query("select * from employees");
+    console.table(result[0]);
 
+    const resultUpdate = await conn.query(
+      "update employees set name = ? where name = ?",
+      ["Ashwini Mishra", "Ashwini"]
+    );
+    console.log(resultUpdate[0].affectedRows);
 
-        const resultUpdate = await conn.query(
-            "update employees set name = ? where name = ?",["Ashwini Mishra","Ashwini"]
-        );
-        console.log(resultUpdate[0].affectedRows);
+    await conn.commit();
 
-        await conn.commit();    
+    const [row, schema] = await conn.query("select * from employees");
+    console.table(row);
 
-        const [row,schema] = await conn.query("select * from employees");
-        console.table(row);
-
-        const ssn = "116"
-        const emp = await conn.query(`select * from employees where ssn = ?`,[ssn]);
-        console.table(emp[0]);
-    }
-    catch(ex){
-        console.error(ex);
-    }
+    const ssn = "116";
+    const emp = await conn.query(`select * from employees where ssn = ?`, [
+      ssn,
+    ]);
+    console.table(emp[0]);
+  } catch (ex) {
+    console.error(ex);
+  }
 }
